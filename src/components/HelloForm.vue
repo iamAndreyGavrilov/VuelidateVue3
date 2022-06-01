d25a94530b4c15e516d68beb0d7824172432182c
Andrey Gavrilov <a.gavrilov@promo-bot.ru>
2022-06-01 16:03:00 +0500

    скелет верстки

--- src/components/HelloForm.vue
+++ src/components/HelloForm.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>hi</div>
+  <div>
+    <form>
+      <h3>Пароль</h3>
+      <p><input v-model.trim="password" type="password" /></p>
+      <h3>Подтверждение пароля</h3>
+      <p><input v-model.trim="passwordСheck" type="password" /></p>
+      <button @click="submitForm" type="submit">Отправить</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HelloForm",
-  props: {},
+  data() {
+    return {
+      password: "",
+      passwordСheck: "",
+    };
+  },
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+    },
+  },
 };
 </script>
 
